9da595fa3523bfb0122b47c2c29669e579ff4114
Rich Greenhill <rgnet1@yahoo.com>
2015-12-29 09:35:40 -0800

    written post skeleton, added gulp watch

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
-import FlashMessage from "./flash-message";
+let Post = {
+  findAll(){
+    return new Promise( (resolve, reject) => {
+      resolve('ok posts!');
+    } );    
+  }
+};
 
-let flash = new FlashMessage("Hello from ES2015, Babel and Gulp!");
-flash.display();
+let ui = {
+  renderPosts(posts){
+    console.log(posts);
+  }  
+};
 
+
+Post.findAll().then(ui.renderPosts);
