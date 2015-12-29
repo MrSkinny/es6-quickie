7b6acecf266116cf2c40cfce09c9b53c9038599f
Rich Greenhill <rgnet1@yahoo.com>
2015-12-29 10:00:37 -0800

    added api call, error catch

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,10 +1,24 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+"use strict";
 
 var Post = {
   findAll: function findAll() {
     return new Promise(function (resolve, reject) {
-      resolve('ok posts!');
+      var uri = "http://localhost:3000/posts";
+      var request = new XMLHttpRequest();
+
+      request.open("GET", uri, true); // 'true' async, won't block
+      request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+          resolve(JSON.parse(request.response));
+        }
+      };
+
+      request.onerror = function () {
+        reject(new Error("Something went wrong with API"));
+      };
+
+      request.send();
     });
   }
 };
@@ -15,6 +29,8 @@ var ui = {
   }
 };
 
-Post.findAll().then(ui.renderPosts);
+Post.findAll().then(ui.renderPosts).catch(function (error) {
+  return console.log(error);
+});
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 let Post = {
   findAll(){
     return new Promise( (resolve, reject) => {
-      resolve('ok posts!');
+      let uri = "http://localhost:3000/posts";
+      let request = new XMLHttpRequest();
+      
+      request.open("GET", uri, true); // 'true' async, won't block
+      request.onload = () => {
+        if(request.status >= 200 && request.status < 400){
+          resolve(JSON.parse(request.response));
+        }
+      };
+      
+      request.onerror = () => {
+        reject( new Error("Something went wrong with API") );
+      };
+      
+      request.send();
     } );    
   }
 };
@@ -13,4 +27,7 @@ let ui = {
 };
 
 
-Post.findAll().then(ui.renderPosts);
+Post.findAll()
+  .then(ui.renderPosts)
+  .catch( (error) => console.log(error) );
+  
